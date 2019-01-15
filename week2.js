5d71da9a073f34d4c95d494b5be79af6e364a1e8
BipinBikesh <39394847+BipinBikesh@users.noreply.github.com>
2019-01-15 11:37:08 +0530

    Update week2.js

--- week2.js
+++ week2.js
@@ -1,4 +1,6 @@
 //part 1
+/*Write a JavaScript program that modifies an image by putting three vertical 
+stripes on it - a red stripe on the left one third, a green stripe in the middle, and a blue stripe on the right one third. */
 
 var img = new SimpleImage("smallhands.png");
 print(img);
@@ -16,6 +18,8 @@ for (var pixel of img.values()){
 print(img);
 
 //part 2
+/*Write a JavaScript function named swapRedGreen with one parameter pixel (representing a single pixel).
+This function should swap the red and green values of the pixel.*/
 
 var img = new SimpleImage("eastereggs.jpg");
 print(img);
@@ -32,7 +36,7 @@ for ( var pixel of img.values()){
 print(img);
 
 //part 3
-
+/* Write code to change the Duke blue devil (the image below on the left) to be yellow (as in the image below on the right).*/
 
 var img = new SimpleImage("duke_blue_devil.png");
 print(img);
